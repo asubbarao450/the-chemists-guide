76598ecc6adce35a5c769e2613d8f72d8e6f5166
Anirudh Subbarao <asubbarao450@gmail.com>
2024-06-25 21:49:27 -0500

    added code for database

--- database.sql
+++ database.sql
@@ -7,3 +7,23 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+
+CREATE TABLE "element" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (80) UNIQUE NOT NULL,
+    "description" VARCHAR (1000) NOT NULL,
+    "atomic_number" integer,
+    "atomic_weight" double
+);
+
+
+CREATE TABLE "compound" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (80) UNIQUE NOT NULL,
+    "description" VARCHAR (1000) NOT NULL,
+    "date" timestamp,
+    "user_id" integer,
+    "image" varchar (1200) UNIQUE NOT NULL,
+    "quantity" double
+);
