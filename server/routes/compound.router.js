9b85902fcd75723993b0006e00fd9b020440ec4d
Anirudh Subbarao <asubbarao450@gmail.com>
2024-06-27 15:20:12 -0500

    added code for landing page

--- server/routes/compound.router.js
+++ server/routes/compound.router.js
@@ -6,7 +6,7 @@ const router = express.Router();
  * GETS All the compounds from the database with all parameters
  */
 router.get('/', (req, res) => {
-  
+
 
   const queryText = `
   SELECT * FROM "compounds"; 
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 
   pool.query(query)
     .then(result => {
-      res.send(result.rows); 
+      res.send(result.rows);
 
     })
 
@@ -33,7 +33,7 @@ router.get('/', (req, res) => {
  * GETS specific compounds from the database with all parameters
  */
 router.get('/:id', (req, res) => {
-  
+
   //req.body returns the 
   let newid = req.params.id
 
@@ -45,7 +45,7 @@ router.get('/:id', (req, res) => {
 
   pool.query(query, [newid])
     .then(result => {
-      res.send(result.rows); 
+      res.send(result.rows);
 
     })
 
@@ -71,45 +71,71 @@ router.post('/', (req, res) => {
 
   const insertCompoundDetails = [
     req.body.name,
-    req.body.description, 
+    req.body.description,
     req.body.user_id,
-    req.body.image, 
+    req.body.image,
     req.body.quantity
 
   ]
 
 
   pool.query(insertCompoundQuery, insertCompoundDetails)
-  .then((result) => { res.sendStatus(201); })
+    .then((result) => { res.sendStatus(201); })
 
-  .catch((err) => {
-    console.log('Error in POST', err);
-    res.sendStatus(500);
-  });
-  
+    .catch((err) => {
+      console.log('Error in POST', err);
+      res.sendStatus(500);
+    });
+
 });
 
 
 /**
- * Creates a compound entry with 5 catagories and a date timestamp
+ * UPDATE compound in database
+ * 
+ * The way 
  */
-router.put('/', (req, res) => {
+router.put('/:id', (req, res) => {
 
-  const insertCompoundQuery = `
-  INSERT INTO "compounds"  
-      ("name", "description", "date", "user_id", "image", "quantity")
-      VALUES
-    ($1, $2, 'NOW()', $3, $4, $5)
-  `
+  //verify what having this statemnt will do
+  //return values that are stored in the server
+  const updateCompounds = req.body;
 
-  const insertCompoundDetails = [
-    req.body.name,
-    req.body.description, 
-    req.body.user_id,
-    req.body.image, 
-    req.body.quantity
+
+  const newCompoundQuery = `
+  UPDATE "compounds"  
+    SET    
+    "name" = $1,
+    "description" = $2,  
+    "date" = $3, 
+    "user_id" = $4, 
+    "image" = $5, 
+    "quantity" = $6
+
+    WHERE 
+    id = $8 ;
+     
+  `;
+
+  //update this statement so that the previous 
+  //values that were stored in the server 
+  //will be kept
+  const newCompoundValues = [
+    updateCompounds.name,
+    updateCompounds.description,
+    updateCompounds.date,
+    updateCompounds.user_id,
+    updateCompounds.image,
+    updateCompounds.quantity
 
   ]
+
+  pool.query(newCompoundQuery, newCompoundValues)
+    .then((result) => { res.sendStatus(200); })
+    .catch((err) => {
+      console.log("put route not working")
+      res.sendStatus(500);
+    })
 
 });
 
@@ -124,13 +150,13 @@ router.delete('/:id', (req, res) => {
   `;
 
   pool.query(queryText, [deleteloc])
-  .then(()=> {res.sendStatus(200);})
-  .catch((err)=> {
+    .then(() => { res.sendStatus(200); })
+    .catch((err) => {
 
-    console.log('Error in Delete Route');
+      console.log('Error in Delete Route');
 
-    res.sendStatus(500);
-  })
+      res.sendStatus(500);
+    })
 
 
 })
